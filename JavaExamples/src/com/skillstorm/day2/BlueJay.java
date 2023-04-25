c1231b4a38465babc1aeec7e2ba40210a29c7ca6
seancarter99 <scarter@skillstorm.com>
2023-04-25 10:59:00 -0400

    Adding Java class comparison file

---
+++ JavaExamples/src/com/skillstorm/day2/BlueJay.java
@@ -0,0 +1,14 @@
+package com.skillstorm.day2;
+
+public class BlueJay {
+
+	public static void chirp() {
+		System.out.println("Chirp Chirp");
+	}
+	
+	public static void main(String[] args) {
+		BlueJay bluejay = new BlueJay();
+		
+		bluejay.chirp();
+	}
+}
