d73bbea2af408a1c1f5e80fb5bb5db07e15101d8
seancarter99 <scarter@skillstorm.com>
2023-06-02 16:30:45 -0400

    Adding Docker and K8s examples

---
+++ spring-docker/src/test/java/com/skillstorm/demo/SpringDockerApplicationTests.java
@@ -0,0 +1,10 @@
+package com.skillstorm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerApplicationTests {
+
+
+}
